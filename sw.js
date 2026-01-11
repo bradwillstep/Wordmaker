64eb7242568a55f308eb10dac3c592a7d8ed17df
bradwillstep <bradleywilliam.stephenson@gmail.com>
2026-01-11 12:43:34 -0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "wordmaker-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    await cache.addAll(ASSETS);
+    self.skipWaiting();
+  })());
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)));
+    self.clients.claim();
+  })());
+});
+
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+  if (url.origin !== location.origin || event.request.method !== "GET") return;
+
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    const cached = await cache.match(event.request, { ignoreSearch: true });
+    if (cached) return cached;
+    const resp = await fetch(event.request);
+    cache.put(event.request, resp.clone()).catch(() => {});
+    return resp;
+  })().catch(async () => {
+    const cache = await caches.open(CACHE_NAME);
+    return (await cache.match("./index.html")) || Response.error();
+  }));
+});
